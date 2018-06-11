228969c88fe06935d1214e278460bf4da5080d2f
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-11 16:11:33 +0300

    Add basic ball movement functionality

---
+++ DIY Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float force = 50;
+    private Rigidbody2D ballRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        ballRigidbody = GetComponent<Rigidbody2D>();
+        ballRigidbody.AddForce(new Vector2(5 * force, force));
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+	}
+}
