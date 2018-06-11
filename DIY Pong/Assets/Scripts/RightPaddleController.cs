e012473b226e8c559eb4856c2d79b7754e6cd3d2
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-11 11:40:57 +0300

    Add simultaneous movement for both players

---
+++ DIY Pong/Assets/Scripts/RightPaddleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightPaddleController : MonoBehaviour {
+
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float inputY = Input.GetAxisRaw("Vertical"); // Input for vertical movement
+        float velocity = inputY * speed; // Velocity is the speed of something in a given direction
+        transform.Translate(Vector2.up * velocity * Time.deltaTime); // Moving the paddle in the direction and distance of translation. Vector2.up is shorthand for writing Vector2(0, 1) or moving vertically
+    }
+}
