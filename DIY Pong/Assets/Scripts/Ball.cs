17d8dcffdbd2f28c3ac3f113fe5251dee80436fe
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-11 17:18:44 +0300

    Add random ball direction functionality. Add force replaced with velocity

--- DIY Pong/Assets/Scripts/Ball.cs
+++ DIY Pong/Assets/Scripts/Ball.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour {
 
-    public float force = 50;
-    private Rigidbody2D ballRigidbody;
+    public float speed = 10;
+    Rigidbody2D ballRigidbody;
+    Vector2 spawnDirection;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         ballRigidbody = GetComponent<Rigidbody2D>();
-        ballRigidbody.AddForce(new Vector2(5 * force, force));
+        //ballRigidbody.AddForce(new Vector2(5 * force, force));
+
+        int randomBallPosition = Random.Range(1, 5);
+
+        if (randomBallPosition == 1) {
+            spawnDirection = new Vector2(1, 1);
+        } else if (randomBallPosition == 2) {
+            spawnDirection = new Vector2(1, -1);
+        } else if (randomBallPosition == 3) {
+            spawnDirection = new Vector2(-1, -1);
+        } else if (randomBallPosition == 4) {
+            spawnDirection = new Vector2(-1, 1);
+        }
+
+        ballRigidbody.velocity = (spawnDirection * speed);
+
     }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         
-	}
+        
+    }
 }
