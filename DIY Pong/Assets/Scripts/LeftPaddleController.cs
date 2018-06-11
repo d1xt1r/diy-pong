a164e77903317922fc29b798eee01af2d8b940a9
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-11 12:01:10 +0300

    Add independent movement functionality

--- DIY Pong/Assets/Scripts/LeftPaddleController.cs
+++ DIY Pong/Assets/Scripts/LeftPaddleController.cs
@@ -6,14 +6,14 @@ public class LeftPaddleController : MonoBehaviour {
 
     public float speed = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float inputY = Input.GetAxisRaw("Vertical"); // Input for vertical movement
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float inputY = Input.GetAxisRaw("Vertical_Left"); // Input for vertical movement
         float velocity = inputY * speed; // Velocity is the speed of something in a given direction
         transform.Translate(Vector2.up * velocity * Time.deltaTime); // Moving the paddle in the direction and distance of translation. Vector2.up is shorthand for writing Vector2(0, 1) or moving vertically
     }
--- DIY Pong/Assets/Scripts/RightPaddleController.cs
+++ DIY Pong/Assets/Scripts/RightPaddleController.cs
@@ -13,7 +13,7 @@ public class RightPaddleController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float inputY = Input.GetAxisRaw("Vertical"); // Input for vertical movement
+        float inputY = Input.GetAxisRaw("Vertical_Right"); // Input for vertical movement
         float velocity = inputY * speed; // Velocity is the speed of something in a given direction
         transform.Translate(Vector2.up * velocity * Time.deltaTime); // Moving the paddle in the direction and distance of translation. Vector2.up is shorthand for writing Vector2(0, 1) or moving vertically
     }
