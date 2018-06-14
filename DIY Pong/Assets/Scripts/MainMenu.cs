19c0d82e74b417fa03e0a85c06bfdfd45d148082
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-14 14:59:50 +0300

    Add sounds on hover, play.

--- DIY Pong/Assets/Scripts/MainMenu.cs
+++ DIY Pong/Assets/Scripts/MainMenu.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour {
 
-    public void PlayGame() {
+    public AudioSource source;
+    public AudioClip hover;
+    public AudioClip click;
+
+    public void Wrapper() {
+        StartCoroutine(PlayGame());
+    }
+
+    public void OnHover() {
+        source.PlayOneShot(hover);
+    }
+
+    public void OnClick() {
+        source.PlayOneShot(click);
+    }
+
+    IEnumerator PlayGame() {
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(1);
+        
     }
 
     public void QuitGame() {
