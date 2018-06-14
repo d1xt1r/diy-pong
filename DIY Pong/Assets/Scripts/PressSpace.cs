8c952efd909f9db4f318139bd6c4d7f2d655b596
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-14 10:46:02 +0300

    Add animation to press start. Press start functionality changed.

--- DIY Pong/Assets/Scripts/PressSpace.cs
+++ DIY Pong/Assets/Scripts/PressSpace.cs
@@ -1,24 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+//using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PressSpace : MonoBehaviour {
 
-    public TextMeshProUGUI pressSpaceText;
+    //public TextMeshProUGUI pressSpaceText;
 
     // Use this for initialization
     void Start () {
-
-        pressSpaceText = GetComponent<TextMeshProUGUI>();
-        Time.timeScale = 0;
-    }
+            //pressSpaceText = GetComponent<TextMeshProUGUI>();
+            //Time.timeScale = 0;
+     }
 
     // Update is called once per frame
     void Update () {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            Destroy(pressSpaceText);
-            Time.timeScale = 1;
+            SceneManager.LoadScene(2);
+            //Destroy(pressSpaceText);
+            //Time.timeScale = 1;
         }
+
     }
 }
