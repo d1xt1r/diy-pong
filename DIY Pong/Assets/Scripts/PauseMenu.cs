14243959c022e767f8f6f5a9f564a7113bb4bb72
Denislav Georgiev <d1xt1r@gmail.com>
2018-06-20 10:40:57 +0300

    Add pause menu with volume slider and option to return to main menu

---
+++ DIY Pong/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pauseMenuUI;
+
+    public static bool GameIsPaused = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+	}
+
+    public void Pause() {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        AudioListener.pause = true;
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        AudioListener.pause = false;
+    }
+
+    public void VolumeLevel(float newValue) {
+        float newVolume = AudioListener.volume;
+        newVolume = newValue;
+        AudioListener.volume = newVolume;
+    }
+
+    public void LoadMainMenu() {
+        AudioListener.pause = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
